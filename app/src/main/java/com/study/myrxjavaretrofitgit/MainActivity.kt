4002b6f50ad4da3fd6013d38339ffefc562fa143
SongShuangPeng <981297079@qq.com>
2020-05-27 14:41:08 +0800

    首次提交

---
+++ app/src/main/java/com/study/myrxjavaretrofitgit/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.study.myrxjavaretrofitgit
+
+import android.os.Bundle
+import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val progressBar = findViewById<ProgressBar>(R.id.cpb)
+        val shown = progressBar.isShown
+    }
+}
